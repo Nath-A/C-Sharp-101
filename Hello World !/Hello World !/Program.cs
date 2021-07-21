214e34f16b5dc0f9eaf70872896034985456bced
Nathalie Anneessens <77076065+nathalie-anneessens@users.noreply.github.com>
2021-07-21 19:04:01 +0200

    A lot of files. No idea where there are from

---
+++ Hello World !/Hello World !/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_World__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
