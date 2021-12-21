023a41b9a8f5abcb9fec99b968f223c51fbb4167
Nathalie Anneessens <nathalie.anneessens@gmail.com>
2021-12-21 18:44:14 +0100

    C# Fundamentals : Looping through Array [Started]

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,6 +6,14 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var numbers = new double[3];
+            numbers[0] = 6.66;
+            numbers[1] = 66.6;
+            numbers[2] = 666;
+
+            
+            
+            
             if (args.Length > 0)
             {
                 Console.WriteLine($"Hello {args[0]} !");
