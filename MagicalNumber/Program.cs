94d5ef648b2b71a5f9672c2b4863c70d655381dc
Nathalie Anneessens <77076065+nathalie-anneessens@users.noreply.github.com>
2022-01-31 22:03:43 +0100

    On revoit les bases

---
+++ MagicalNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MagicalNumber
+{
+    class Program
+    {
+        static int AskNumber()
+        {
+
+
+            Console.Write("Give me a number");
+            string StringNumber = Console.ReadLine();
+
+            try
+            {
+                int IntNumber = int.Parse(StringNumber);
+
+            }
+
+            catch
+            {
+
+            }
+
+            //if (Number == 0)
+            //{
+            //    Console.WriteLine("Error, 0 is not a valid number...");
+            //    Console.WriteLine("Give me a correct number");
+            //    StringNumber = Console.ReadLine();
+            //}
+
+            //Console.WriteLine(StringNumber);
+            //return Number;
+
+        }
+        static void Main(String[] args)
+        {
+            
+
+            AskNumber();
+        }
+    }
+}
